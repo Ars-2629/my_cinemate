ede43735b3fbccc7b254be2fd040c88a5904bc60
Mohd Aazib <rahil.aazib@gmail.com>
2023-08-19 00:48:52 +0530

    updated margin to adjust cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,7 @@ const {id,original_title,overview,poster_path}=movie;
 const image = poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : Backup ;
 
   return (
-      <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
 
     <Link to={`/movie/${id}`}>
         <img className="rounded-t-lg" src={image} alt="" />
