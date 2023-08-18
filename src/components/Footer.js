0a3aaaa1528f502e9345dd9e5084748ebba2a5f3
Mohd Aazib <rahil.aazib@gmail.com>
2023-08-19 00:12:25 +0530

    updated href with a proper value

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,16 @@ export function Footer() {
 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.</span>
 <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
     <li>
-        <a href="#" target="_blank" rel='noreferrer' className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+        <a href="https://www.facebook.com/rahilsiddiqui5859?mibextid=ZbWKwL" target="_blank" rel='noreferrer' className="mr-4 hover:underline md:mr-6 ">Instagram</a>
     </li>
     <li>
-        <a href="#" target="_blank"  rel='noreferrer' className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/mohammad-aazib-a17636125" target="_blank"  rel='noreferrer' className="mr-4 hover:underline md:mr-6">LinkedIn</a>
     </li>
     <li>
-        <a href="#" target="_blank" rel='noreferrer' className="mr-4 hover:underline md:mr-6">Youtube</a>
+        <a href="https://instagram.com/rahil6546?igshid=MzNlNGNkZWQ4Mg==" target="_blank" rel='noreferrer' className="mr-4 hover:underline md:mr-6">Youtube</a>
     </li>
     <li>
-        <a href="#" target="_blank" className="hover:underline">Github</a>
+        <a href="https://github.com/Ars-2629" target="_blank" className="hover:underline">Github</a>
     </li>
 </ul>
 </footer>
