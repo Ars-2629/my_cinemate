ff56843cdb450708dede544507a73e818db88a46
Mohd Aazib <rahil.aazib@gmail.com>
2023-08-19 15:23:14 +0530

    App title change

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ export function Header() {
           <Link to="/" className="flex items-center">
             <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Cinemate Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Cinemate
+              Cinema Mate
             </span>
           </Link>
 
